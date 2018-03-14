abf8784aafd9e5b6cc7e3d3d74ded468c5644617
Jonathan Harrison <j.harrison@altitudeangel.com>
2018-03-14 10:02:26 +0000

    WIP

---
+++ KubeMob/KubeMob.Common.UnitTests/Behaviors/EventToCommandBehaviorTests.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace KubeMob.Common.UnitTests.Behaviors
+{
+    public class EventToCommandBehaviorTests
+	{
+		[Fact]
+		public void InvalidEventNameShouldThrowArgumentExceptionText()
+		{
+			Assert.True(false);
+		}
+	}
+}

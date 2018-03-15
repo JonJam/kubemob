36858493cd74994fa019d23e7b63f192b55dd9d4
Jonathan Harrison <j.harrison@altitudeangel.com>
2018-03-15 07:48:05 +0000

    Updating UI Test

--- KubeMob/KubeMob.UITests/Tests.cs
+++ KubeMob/KubeMob.UITests/Tests.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using System.IO;
-using System.Linq;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 using Xamarin.UITest;
-using Xamarin.UITest.Queries;
 
 namespace KubeMob.UITests
 {
     [TestFixture(Platform.Android)]
-    //[TestFixture(Platform.iOS)]
+    [TestFixture(Platform.iOS)]
     public class Tests
     {
         IApp app;

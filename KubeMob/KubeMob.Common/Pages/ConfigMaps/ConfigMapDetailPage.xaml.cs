b0ee1c369fe96da76c346e4d28397488bc6a9349
Jonathan Harrison <j.harrison@altitudeangel.com>
2018-06-07 12:15:56 +0100

    WIP - config map

---
+++ KubeMob/KubeMob.Common/Pages/ConfigMaps/ConfigMapDetailPage.xaml.cs
@@ -0,0 +1,11 @@
+using KubeMob.Common.Pages.Base;
+using Xamarin.Forms.Internals;
+
+namespace KubeMob.Common.Pages.ConfigMaps
+{
+    public partial class ConfigMapDetailPage : ExtendedContentPage
+    {
+        [Preserve]
+        public ConfigMapDetailPage() => this.InitializeComponent();
+    }
+}
---
+++ KubeMob/KubeMob.Common/Services/Kubernetes/Model/ConfigMapDetail.cs
@@ -0,0 +1,93 @@
+using System.Collections.Generic;
+using Xamarin.Forms.Internals;
+
+namespace KubeMob.Common.Services.Kubernetes.Model
+{
+    [Preserve(AllMembers = true)]
+    public class ConfigMapDetail
+    {
+        public ConfigMapDetail()
+            //string name,
+            //string namespaceName,
+            //IReadOnlyList<string> labels,
+            //IReadOnlyList<string> annotations,
+            //string creationTime,
+            //string schedule,
+            //bool suspend,
+            //string lastSchedule,
+            //string concurrencyPolicy,
+            //string startingDeadlineSeconds,
+            //IReadOnlyList<OwnerReference> activeJobs)
+        {
+            //this.Name = name;
+            //this.NamespaceName = namespaceName;
+            //this.Labels = labels;
+            //this.Annotations = annotations;
+            //this.CreationTime = creationTime;
+            //this.Schedule = schedule;
+            //this.Suspend = suspend;
+            //this.LastSchedule = lastSchedule;
+            //this.ConcurrencyPolicy = concurrencyPolicy;
+            //this.StartingDeadlineSeconds = startingDeadlineSeconds;
+            //this.ActiveJobs = activeJobs;
+        }
+
+        public string Name
+        {
+            get;
+        }
+
+        public string NamespaceName
+        {
+            get;
+        }
+
+        public IReadOnlyList<string> Labels
+        {
+            get;
+        }
+
+        public IReadOnlyList<string> Annotations
+        {
+            get;
+        }
+
+        public string CreationTime
+        {
+            get;
+        }
+
+        public string Schedule
+        {
+            get;
+        }
+
+        public bool Suspend
+        {
+            get;
+        }
+
+        public string LastSchedule
+        {
+            get;
+        }
+
+        public string ConcurrencyPolicy
+        {
+            get;
+        }
+
+        public string StartingDeadlineSeconds
+        {
+            get;
+        }
+
+        // TODO Handle links
+        public IReadOnlyList<OwnerReference> ActiveJobs
+        {
+            get;
+        }
+
+        public int ActiveJobCount => this.ActiveJobs.Count;
+    }
+}

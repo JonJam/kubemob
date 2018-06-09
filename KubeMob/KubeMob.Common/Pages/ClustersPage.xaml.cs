af0215aff378b47db1ef25358c7971629c1bd41b
Jonathan Harrison <jonjam1990@googlemail.com>
2018-06-10 00:52:34 +0100

    Updated listview for clusters

--- KubeMob/KubeMob.Common/Pages/ClustersPage.xaml.cs
+++ KubeMob/KubeMob.Common/Pages/ClustersPage.xaml.cs
@@ -1,5 +1,4 @@
-﻿using KubeMob.Common.Pages.Base;
-using Xamarin.Forms;
+using KubeMob.Common.Pages.Base;
 using Xamarin.Forms.Internals;
 
 namespace KubeMob.Common.Pages
@@ -7,15 +6,6 @@ namespace KubeMob.Common.Pages
     public partial class ClustersPage : ExtendedContentPage
     {
         [Preserve]
-        public ClustersPage()
-        {
-            this.InitializeComponent();
-
-            // TODO Try refactor this into ViewModel ??
-            this.Clusters.ItemSelected += this.OnSelected;
-            this.Accounts.ItemSelected += this.OnSelected;
-        }
-
-        private void OnSelected(object sender, SelectedItemChangedEventArgs e) => ((ListView)sender).SelectedItem = null;
+        public ClustersPage() => this.InitializeComponent();
     }
 }

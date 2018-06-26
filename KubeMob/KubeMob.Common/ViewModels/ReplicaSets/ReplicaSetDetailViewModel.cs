4380d8d99fdf218f0e1820c64dedb964f3277140
Jonathan Harrison <j.harrison@altitudeangel.com>
2018-06-26 16:53:34 +0100

    Implemented related pods

--- KubeMob/KubeMob.Common/ViewModels/ReplicaSets/ReplicaSetDetailViewModel.cs
+++ KubeMob/KubeMob.Common/ViewModels/ReplicaSets/ReplicaSetDetailViewModel.cs
@@ -1,9 +1,11 @@
 using System.Threading.Tasks;
+using System.Windows.Input;
 using KubeMob.Common.Services.Kubernetes;
 using KubeMob.Common.Services.Kubernetes.Model;
 using KubeMob.Common.Services.Navigation;
 using KubeMob.Common.Services.Popup;
 using KubeMob.Common.ViewModels.Base;
+using Xamarin.Forms;
 using Xamarin.Forms.Internals;
 
 namespace KubeMob.Common.ViewModels.ReplicaSets
@@ -17,8 +19,21 @@ namespace KubeMob.Common.ViewModels.ReplicaSets
             INavigationService navigationService)
             : base(kubernetesService, popupService, navigationService)
         {
+            this.ViewRelatedPodsCommand = new Command(async () => await this.OnViewRelatedPodsCommandExecute());
+        }
+
+        public ICommand ViewRelatedPodsCommand
+        {
+            get;
         }
 
         protected override Task<ReplicaSetDetail> GetObjectDetail(string name, string namespaceName) => this.KubernetesService.GetReplicaSetDetail(name, namespaceName);
+
+        private Task OnViewRelatedPodsCommandExecute()
+        {
+            Filter filter = new Filter(other: this.Name);
+
+            return this.NavigationService.NavigateToPodsPage(filter);
+        }
     }
 }

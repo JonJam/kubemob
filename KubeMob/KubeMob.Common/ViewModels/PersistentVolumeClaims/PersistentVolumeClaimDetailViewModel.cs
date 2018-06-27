bbcae6698de8a63412588d94868e720ed870b38e
Jonathan Harrison <jonjam1990@googlemail.com>
2018-06-28 00:47:06 +0100

    - Handle object not found - link to storage class.

--- KubeMob/KubeMob.Common/ViewModels/PersistentVolumeClaims/PersistentVolumeClaimDetailViewModel.cs
+++ KubeMob/KubeMob.Common/ViewModels/PersistentVolumeClaims/PersistentVolumeClaimDetailViewModel.cs
@@ -1,9 +1,11 @@
 using System.Threading.Tasks;
+using System.Windows.Input;
 using KubeMob.Common.Services.Kubernetes;
 using KubeMob.Common.Services.Kubernetes.Model;
 using KubeMob.Common.Services.Navigation;
 using KubeMob.Common.Services.Popup;
 using KubeMob.Common.ViewModels.Base;
+using Xamarin.Forms;
 using Xamarin.Forms.Internals;
 
 namespace KubeMob.Common.ViewModels.PersistentVolumeClaims
@@ -17,8 +19,17 @@ namespace KubeMob.Common.ViewModels.PersistentVolumeClaims
             INavigationService navigationService)
             : base(kubernetesService, popupService, navigationService)
         {
+            this.NavigateToStorageClassCommand = new Command(async () => await this.OnNavigateToStorageClassCommandExecute());
         }
 
+        public ICommand NavigateToStorageClassCommand
+        {
+            get;
+        }
+        
         protected override Task<PersistentVolumeClaimDetail> GetObjectDetail(string name, string namespaceName) => this.KubernetesService.GetPersistentVolumeClaimDetail(name, namespaceName);
+        
+        private Task OnNavigateToStorageClassCommandExecute() => this.NavigationService.NavigateToStorageClassDetailPage(
+            this.Detail.StorageClass);
     }
 }

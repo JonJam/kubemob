c29548d0a9b1903e90d781cb995252b069d29c3a
Jonathan Harrison <jonjam1990@googlemail.com>
2018-09-10 22:09:05 +0100

    Styled deployment detail.

--- KubeMob/KubeMob.Common/Pages/Deployments/DeploymentDetailTabbedPage.xaml.cs
+++ KubeMob/KubeMob.Common/Pages/Deployments/DeploymentDetailTabbedPage.xaml.cs
@@ -1,10 +1,8 @@
 using KubeMob.Common.Services.Kubernetes.Model;
-using KubeMob.Common.ViewModels.CronJobs;
-using KubeMob.Common.ViewModels.DaemonSets;
+using KubeMob.Common.ViewModels.Deployments;
 using KubeMob.Common.ViewModels.Events;
-using KubeMob.Common.ViewModels.Jobs;
-using KubeMob.Common.ViewModels.Pods;
-using KubeMob.Common.ViewModels.Services;
+using KubeMob.Common.ViewModels.HorizontalPodAutoscalers;
+using KubeMob.Common.ViewModels.ReplicaSets;
 using Xamarin.Forms;
 using Xamarin.Forms.Internals;
 
@@ -15,42 +13,41 @@ namespace KubeMob.Common.Pages.Deployments
         [Preserve]
         public DeploymentDetailTabbedPage() => this.InitializeComponent();
 
-        // TODO tabs
-        //protected override async void OnCurrentPageChanged()
-        //{
-        //    base.OnCurrentPageChanged();
+        protected override async void OnCurrentPageChanged()
+        {
+            base.OnCurrentPageChanged();
 
-        //    Page page = this.CurrentPage;
+            Page page = this.CurrentPage;
 
-        //    DaemonSetDetailViewModel detailViewModel = (DaemonSetDetailViewModel)this.DetailPage.BindingContext;
+            DeploymentDetailViewModel detailViewModel = (DeploymentDetailViewModel)this.DetailPage.BindingContext;
 
-        //    // detailViewModel.Detail will be null if an error occurs which will have already been handled, so
-        //    // do nothing. Otherwise try load events.
-        //    if (page.BindingContext is EventsViewModel eventsViewModel &&
-        //        detailViewModel.Detail != null)
-        //    {
-        //        Filter filter = new Filter(detailViewModel.Detail.NamespaceName, other: detailViewModel.Detail.Uid);
+            // detailViewModel.Detail will be null if an error occurs which will have already been handled, so
+            // do nothing. Otherwise try load events.
+            if (page.BindingContext is EventsViewModel eventsViewModel &&
+                detailViewModel.Detail != null)
+            {
+                Filter filter = new Filter(detailViewModel.Detail.NamespaceName, other: detailViewModel.Detail.Uid);
 
-        //        await eventsViewModel.Initialize(filter);
-        //    }
-        //    else if (page.BindingContext is ServicesViewModel servicesViewModel &&
-        //             detailViewModel.Detail != null)
-        //    {
-        //        Filter filter = new Filter(
-        //            detailViewModel.Detail.NamespaceName,
-        //            other: detailViewModel.Detail.RelatedSelector);
+                await eventsViewModel.Initialize(filter);
+            }
+            else if (page.BindingContext is ReplicaSetsViewModel replicaSetsViewModel &&
+                     detailViewModel.Detail != null)
+            {
+                Filter filter = new Filter(
+                    detailViewModel.Detail.NamespaceName,
+                    other: detailViewModel.Detail.Uid);
 
-        //        await servicesViewModel.Initialize(filter);
-        //    }
-        //    else if (page.BindingContext is PodsViewModel podsViewModel &&
-        //             detailViewModel.Detail != null)
-        //    {
-        //        Filter filter = new Filter(
-        //            detailViewModel.Detail.NamespaceName,
-        //            other: detailViewModel.Detail.Uid);
+                await replicaSetsViewModel.Initialize(filter);
+            }
+            else if (page.BindingContext is HorizontalPodAutoscalersViewModel horizontalPodAutoscalersViewModel &&
+                     detailViewModel.Detail != null)
+            {
+                Filter filter = new Filter(
+                    detailViewModel.Detail.NamespaceName,
+                    other: detailViewModel.Detail.Name);
 
-        //        await podsViewModel.Initialize(filter);
-        //    }
-        //}
+                await horizontalPodAutoscalersViewModel.Initialize(filter);
+            }
+        }
     }
 }
